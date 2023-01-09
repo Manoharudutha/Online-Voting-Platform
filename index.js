649b3bc6aa16110a6aa0a00434436ed32eddd014
Manohar Udutha <manoharudutha@gmail.com>
2023-01-09 19:06:47 +0530

    Elections and options adding functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Started express sever on port ${PORT}`);
 });
