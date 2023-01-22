1f77ec4a13f6682a7cdc7692c05d9b686b553ed2
Manohar Udutha <manoharudutha@gmail.com>
2023-01-23 01:58:33 +0530

    Adding option functionality for ballot of elections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 22000;
+const PORT = process.env.PORT || 54000;
 app.listen(PORT, () => {
   console.log(`Started express sever on port ${PORT}`);
 });
